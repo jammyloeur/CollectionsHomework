ce4f467b0f477e4e7b77dc662fef2c6464aec90b
Jammy Loeur <jammyloeur@csus.edu>
2018-03-02 12:14:35 -0800

    Implemented readFile method.

--- CollectionsHomework/src/Analyzer.java
+++ CollectionsHomework/src/Analyzer.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,14 +16,85 @@ import java.util.Set;
 public class Analyzer {
 	
 	/*
+	 * Check whether String is an Integer
+	 */
+	public static boolean isInteger(String s) {
+		return isInteger(s, 10);
+	}
+	
+	public static boolean isInteger(String s, int radix) {
+		if (s.isEmpty()) return false;
+		for (int i = 0; i < s.length(); i++) {
+			if (i == 0 && s.charAt(i) == '-') {
+				if (s.length() == 1) return false;
+				else continue;
+			}
+			if (Character.digit(s.charAt(i), radix) < 0) return false;
+		}
+		return true;
+	}
+	
+	/*
 	 * Implement this method in Part 1
 	 */
 	public static List<Sentence> readFile(String filename) {
 
 		/* IMPLEMENT THIS METHOD! */
+		List<Sentence> sentenceList = new ArrayList<Sentence>();
 		
-		return null; // this line is here only so this code will compile if you don't modify it
-
+		String line = null;
+		
+		try {
+			// FileReader reads text files in the default encoding.
+			if (filename == null) {
+				return sentenceList;
+			}
+			
+			// Open file to be read.
+			FileReader fileReader = new FileReader(filename);
+			
+			// Always wrap FileReader in BufferedReader
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while ((line = bufferedReader.readLine()) != null) {
+				
+				// Tokenize string to accurately retrieve score.
+				String[] tokens = line.split(" ");
+				if (tokens.length <= 1) {
+					continue;
+				}
+				
+				// Check whether score is an integer.
+				if (isInteger(tokens[0])) {
+					// Check whether score is between -2 to 2 inclusive.
+					if (Integer.parseInt(tokens[0]) >= -2 
+							&& Integer.parseInt(tokens[0]) <= 2) {		
+						// Check whether text is null.
+						if (line.substring(line.indexOf(" ")) != null) {
+							Sentence currentSentence = new Sentence(0, null);
+							// Assign score to current sentence object.
+							currentSentence.score = Integer.parseInt(tokens[0]);
+							// Assign text to current sentence object.
+							currentSentence.text = line.substring(line.indexOf(" ")).trim();
+							// Add sentence object to list.
+							sentenceList.add(currentSentence);
+						}
+					}
+				}
+			}
+			
+			// Always close files.
+			bufferedReader.close();
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + filename + "'");
+		}
+		catch (IOException ex) {
+			System.out.println("Error reading file '" + filename + "'");
+		}
+		
+		return sentenceList; // Return to main method.
+		
 	}
 	
 	/*
